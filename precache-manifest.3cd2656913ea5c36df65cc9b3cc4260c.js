0a158321a7941a5955bb387fed3c545482832196
Alejandro Busse <al3busse@gmail.com>
2020-05-19 06:24:53 -0300

    Updates

---
+++ precache-manifest.3cd2656913ea5c36df65cc9b3cc4260c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d667f28a0adc13b3c337216304815c",
+    "url": "/Javascript-Calculator/index.html"
+  },
+  {
+    "revision": "f898eb513215340c8896",
+    "url": "/Javascript-Calculator/static/css/main.9dfb15b1.chunk.css"
+  },
+  {
+    "revision": "327825af6b9b91584dcb",
+    "url": "/Javascript-Calculator/static/js/2.68fc89ab.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Javascript-Calculator/static/js/2.68fc89ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f898eb513215340c8896",
+    "url": "/Javascript-Calculator/static/js/main.932e968d.chunk.js"
+  },
+  {
+    "revision": "2b0d3ddbc89494e60b80",
+    "url": "/Javascript-Calculator/static/js/runtime-main.41d23104.js"
+  }
+]);
