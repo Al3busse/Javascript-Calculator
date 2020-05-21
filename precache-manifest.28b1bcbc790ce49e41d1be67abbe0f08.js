bfd39c23e15d1ad2020c9b2ea3978422fb9f0b91
Alejandro Busse <al3busse@gmail.com>
2020-05-21 01:44:39 -0300

    Updates

---
+++ precache-manifest.28b1bcbc790ce49e41d1be67abbe0f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f803cb66a028f0b0877b922c2f13585",
+    "url": "/Javascript-Calculator/index.html"
+  },
+  {
+    "revision": "24a8e85ebfc28695c54c",
+    "url": "/Javascript-Calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6f7f37afd34807aebcee",
+    "url": "/Javascript-Calculator/static/css/main.3853260e.chunk.css"
+  },
+  {
+    "revision": "24a8e85ebfc28695c54c",
+    "url": "/Javascript-Calculator/static/js/2.5e100839.chunk.js"
+  },
+  {
+    "revision": "26cea779844b95048ab44bfc1acedf77",
+    "url": "/Javascript-Calculator/static/js/2.5e100839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7f37afd34807aebcee",
+    "url": "/Javascript-Calculator/static/js/main.695ac2d0.chunk.js"
+  },
+  {
+    "revision": "2b0d3ddbc89494e60b80",
+    "url": "/Javascript-Calculator/static/js/runtime-main.41d23104.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Javascript-Calculator/precache-manifest.3cd2656913ea5c36df65cc9b3cc4260c.js"
+  "/Javascript-Calculator/precache-manifest.28b1bcbc790ce49e41d1be67abbe0f08.js"
 );
 
 self.addEventListener('message', (event) => {
